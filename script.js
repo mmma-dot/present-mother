bcf0bfadfd574f51cc19e005494ef39f76dc7c10
mmma-dot <urentsev05@mail.ru>
2025-03-10 14:50:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showSurprise() {
+    let surprise = document.getElementById("surprise");
+    surprise.classList.remove("hidden");
+    surprise.style.animation = "fadeIn 2s ease-in-out";
+}
